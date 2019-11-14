86ecad19c056d496d11a426bfeaa6606b7301fdd
synzen <michael994664@gmail.com>
2019-11-14 00:06:03 -0500

    Fix insert crash due to included _id field, and writing during restoring

--- app/src/main/java/com/example/testapplication/FetchMessages.kt
+++ app/src/main/java/com/example/testapplication/FetchMessages.kt
@@ -1,24 +1,33 @@
 package com.example.testapplication
 
 import android.app.IntentService
+import android.content.ContentValues
 import android.content.Intent
 import android.content.Context
+import android.database.Cursor
 import android.net.Uri
-import android.os.SystemClock
+import android.os.Environment
 import android.provider.Telephony
+import android.util.JsonReader
+import android.util.JsonToken
+import android.util.JsonWriter
 import android.util.Log
+import androidx.documentfile.provider.DocumentFile
+import java.io.File
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
 // TODO: Rename actions, choose action names that describe tasks that this
 // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-private const val FETCH_SMS = "com.example.testapplication.action.FETCH_SMS"
-private const val ACTION_BAZ = "com.example.testapplication.action.BAZ"
+private const val WRITE_SMS = "com.example.testapplication.action.WRITE_SMS"
+private const val RESTORE_SMS = "com.example.testapplication.action.RESTORE_SMS"
 
 // TODO: Rename parameters
-private const val EXTRA_PARAM1 = "com.example.testapplication.extra.PARAM1"
-private const val EXTRA_PARAM2 = "com.example.testapplication.extra.PARAM2"
+private const val EXTRA_PARAM_FILE_PATH = "com.example.testapplication.extra.FILE_PATH"
 
-var FETCHING_SMS = false
-
+var WRITING_SMS = false
+var RESTORING_SMS = false
 /**
  * An [IntentService] subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
@@ -26,54 +35,189 @@ var FETCHING_SMS = false
  * helper methods.
  */
 class FetchMessages : IntentService("FetchMessages") {
-
-    fun getSMS (): String {
-        val cursor = contentResolver.query(Uri.parse("content://sms"), null, null, null, null)
-        var msgData = ""
-        var sendProgress = 0
+    private fun recordRows(cursor: Cursor, jsonWriter: JsonWriter, typeReference: Map<String, Int>) {
+        val total = cursor.count
         var i = 0
-
-        if (cursor?.moveToFirst() == true) { // must check the result to prevent exception
-            val total = cursor.count
-            do {
-                for (idx in 0 until cursor.columnCount) {
-//                    Log.d("TAG", "continuing this column ${cursor.getColumnName(idx)} ${idx}/${cursor.columnCount}")
-                    msgData += " " + cursor.getColumnName(idx) + ":" + cursor.getString(idx)
+        var sendProgress = 0
+        do {
+            jsonWriter.beginObject()
+            for (idx in 0 until cursor.columnCount) {
+                val columnName = cursor.getColumnName(idx)
+                Log.d("TAG", "name: $columnName, value: ${cursor.getString(idx)}")
+                if (!typeReference.containsKey(columnName)) {
+                    continue
                 }
-                var floatProgress = (i++.toFloat()/total) * 100
-                var intProgress = floatProgress.toInt()
-                Log.d("TAG", "continuing cursor ${i}/${total}, ${floatProgress} ${intProgress}")
-                if (intProgress > sendProgress) {
-                    sendProgress = intProgress
-                    sendBroadcast(Intent(SMS_FETCHING_UPDATE_ACTION).apply {
-                        putExtra(SMS_FETCHING_UPDATE_ACTION, sendProgress)
-                    })
+                jsonWriter.name(columnName)
+                if (typeReference[columnName] == Cursor.FIELD_TYPE_INTEGER) {
+                    jsonWriter.value(cursor.getInt(idx))
+                } else {
+                    jsonWriter.value(cursor.getString(idx))
                 }
+            }
+            Log.d("TAG", "${i++}/$total")
+            val floatProgress = (i++.toFloat()/total) * 100
+            val intProgress = floatProgress.toInt()
+            jsonWriter.endObject()
+            if (intProgress > sendProgress) {
+                sendProgress = intProgress
+                sendBroadcast(Intent(SMS_FETCHING_UPDATE_ACTION).apply {
+                    putExtra(SMS_FETCHING_UPDATE_ACTION, sendProgress)
+                })
+            }
+        } while (cursor.moveToNext())
+    }
 
-
-
-                // use msgData
-            } while (cursor.moveToNext())
+    private fun writeToFile(pathToWriteTo: Uri, fileName: String) {
+        val rw = Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
+        if (rw) {
+            Log.d("TAG", "yes")
         } else {
-            // empty box, no SMS
-            Log.d("TAG", "no sms!")
+            Log.d("TAG", "no")
         }
-        cursor?.close()
-        Log.d("TAG", msgData)
-        return msgData
+        if (pathToWriteTo == Uri.EMPTY) {
+            Log.d("TAG", "Empty")
+            return
+        }
+
+        val folderDocument = DocumentFile.fromTreeUri(this, pathToWriteTo)
+        if (folderDocument == null) {
+            Log.d("TAG", "folder does not exist")
+            return
+        }
+
+        var foundFile = folderDocument.findFile("$fileName.json")
+        if (foundFile == null) {
+            foundFile = folderDocument.findFile(FILE_SMS_BACKUP) // Sometimes the json is not appended while writing
+        }
+        if (foundFile != null && !foundFile.delete()) {
+            Log.d("TAG", "file already exists and was unable to be deleted")
+            return
+        }
+        val fileURI = folderDocument.createFile("application/json", fileName)?.uri
+        if (fileURI == null) {
+            Log.d("TAG", "null file URI")
+            return
+        }
+        Log.d("TAG", "file path is ${fileURI.path}")
+        val outputStream = contentResolver.openOutputStream(fileURI)
+        if (outputStream == null) {
+            Log.d("TAG", "null output stream")
+            return
+        }
+        val jsonWriter = JsonWriter(OutputStreamWriter(outputStream))
+        val cursor = contentResolver.query(Telephony.Sms.CONTENT_URI, null, null, null, null)
+        if (cursor == null || !cursor.moveToFirst()) {
+            Log.d("tag", "null cursor")
+            return
+        }
+        val mmsCursor = contentResolver.query(Telephony.Mms.CONTENT_URI, null, null, null, null)
+        Log.d("TAG", "write success")
+        jsonWriter.run {
+            beginObject()
+            name("sms")
+            beginArray()
+            recordRows(cursor, this, SMS_TABLE_TYPES)
+            cursor.close()
+            endArray()
+            name("mms")
+            beginArray()
+            if (mmsCursor != null && mmsCursor.moveToFirst()) {
+                recordRows(mmsCursor, this, MMS_TABLE_TYPES)
+            }
+            mmsCursor?.close()
+            endArray()
+            endObject()
+            flush()
+            close()
+        }
+    }
+
+    private fun readFile(path: Uri, fileName: String) {
+        // Store the column types by column name
+        if (path == Uri.EMPTY) {
+            Log.d("TAG", "Empty path")
+            return
+        }
+
+        // Open the file
+        val dir = DocumentFile.fromTreeUri(this, path)
+        if (dir == null) {
+            Log.d("TAG", "document file null for read")
+            return
+        }
+
+        val file: DocumentFile? = dir.findFile("$fileName.json") ?: dir.findFile(fileName)
+        if (file == null) {
+            Log.d("TAG", "No file found")
+            return
+        }
+
+        val inputStream: InputStream? = contentResolver.openInputStream(file.uri)
+        if (inputStream == null) {
+            Log.d("TAG", "null input stream")
+            return
+        }
+
+        //Log.d("TAG", "data: ${File("${file.uri.toS}.json").bufferedReader().readLines()}")
+        val rows: MutableList<ContentValues> = mutableListOf()
+        JsonReader(InputStreamReader(inputStream)).run {
+            beginObject()
+            nextName() // SMS
+            beginArray()
+            while (hasNext()) {
+                beginObject()
+                val row = ContentValues()
+                while (hasNext()) {
+                    val key = nextName()
+                    if (!SMS_TABLE_TYPES.containsKey(key)) {
+                        skipValue()
+                        continue
+                    }
+
+                    if (SMS_TABLE_TYPES[key] == Cursor.FIELD_TYPE_INTEGER) {
+                        row.put(key, nextInt())
+                    } else if (peek() == JsonToken.NULL) {
+                        Log.d("TAG", "null token? $key")
+                        nextNull()
+                        row.putNull(key)
+                    } else {
+                        val value = nextString()
+                        Log.d("TAG", "Putting ${key}:${value}")
+                        row.put(key, value)
+                    }
+//                    Log.d("json", "key: ${nextName()}")
+//                    Log.d("json", "value: ${nextString()}")
+                }
+                endObject()
+                if (row.size() > 0) {
+                    rows.add(row)
+                }
+            }
+            endArray()
+            nextName() // MMS
+            beginArray()
+            endArray()
+            endObject()
+            close()
+        }
+        val total = rows.count()
+        var i = 0
+        for (row in rows) {
+            Log.d("TAG", "Inserting ${++i}/$total")
+            contentResolver.insert(Telephony.Sms.CONTENT_URI, row)
+        }
+        Log.d("TAG", "insertion complete")
     }
 
     override fun onHandleIntent(intent: Intent?) {
         when (intent?.action) {
-            FETCH_SMS -> {
-                val param1 = intent.getStringExtra(EXTRA_PARAM1)
-                val param2 = intent.getStringExtra(EXTRA_PARAM2)
-                handleActionFoo(param1, param2)
+            WRITE_SMS -> {
+                val filePath = intent.getStringExtra(EXTRA_PARAM_FILE_PATH)
+                handleActionFetchSMS(filePath)
             }
-            ACTION_BAZ -> {
-                val param1 = intent.getStringExtra(EXTRA_PARAM1)
-                val param2 = intent.getStringExtra(EXTRA_PARAM2)
-                handleActionBaz(param1, param2)
+            RESTORE_SMS -> {
+                val filePath = intent.getStringExtra(EXTRA_PARAM_FILE_PATH)
+                handleActionRestoreSMS(filePath)
             }
         }
     }
@@ -82,24 +226,29 @@ class FetchMessages : IntentService("FetchMessages") {
      * Handle action Foo in the provided background thread with the provided
      * parameters.
      */
-    private fun handleActionFoo(param1: String, param2: String) {
-        if (FETCHING_SMS) {
+    private fun handleActionFetchSMS(filePath: String?) {
+        if (WRITING_SMS) {
             Log.d("TAG", "already running fetch sms")
             return
         }
-        FETCHING_SMS = true
+        WRITING_SMS = true
         Log.d("TAG", "Running get sms now.")
-        getSMS()
-        sendBroadcast(Intent(SMS_FETCHED_ACTION))
-        FETCHING_SMS = false
+        writeToFile(Uri.parse(filePath), FILE_SMS_BACKUP)
+        sendBroadcast(Intent(SMS_WRITTEN_ACTION))
+        WRITING_SMS = false
     }
 
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private fun handleActionBaz(param1: String, param2: String) {
-        TODO("Handle action Baz")
+    private fun handleActionRestoreSMS(filePath: String?) {
+        if (RESTORING_SMS) {
+            Log.d("TAG", "already running fetch sms")
+            return
+        }
+        RESTORING_SMS = true
+        Log.d("TAG", "Running get sms now.")
+//        writeToFile(Uri.parse(filePath), FILE_SMS_BACKUP)
+        readFile(Uri.parse(filePath), FILE_SMS_BACKUP)
+        sendBroadcast(Intent(SMS_RESTORED_ACTION))
+        RESTORING_SMS = false
     }
 
     companion object {
@@ -114,30 +263,22 @@ class FetchMessages : IntentService("FetchMessages") {
          */
         // TODO: Customize helper method
         @JvmStatic
-        fun startActionFoo(context: Context, param1: String, param2: String) {
+        fun startActionWriteSMS(context: Context, filePath: Uri) {
             val intent = Intent(context, FetchMessages::class.java).apply {
-                action = FETCH_SMS
-                putExtra(EXTRA_PARAM1, param1)
-                putExtra(EXTRA_PARAM2, param2)
+                action = WRITE_SMS
+                putExtra(EXTRA_PARAM_FILE_PATH, filePath.toString())
             }
-            Log.d("TAG", "starting action")
+            Log.d("TAG", "starting write")
             context.startService(intent)
         }
 
-        /**
-         * Starts this service to perform action Baz with the given parameters. If
-         * the service is already performing a task this action will be queued.
-         *
-         * @see IntentService
-         */
-        // TODO: Customize helper method
         @JvmStatic
-        fun startActionBaz(context: Context, param1: String, param2: String) {
+        fun startActionRestoreSMS(context: Context, filePath: Uri) {
             val intent = Intent(context, FetchMessages::class.java).apply {
-                action = ACTION_BAZ
-                putExtra(EXTRA_PARAM1, param1)
-                putExtra(EXTRA_PARAM2, param2)
+                action = RESTORE_SMS
+                putExtra(EXTRA_PARAM_FILE_PATH, filePath.toString())
             }
+            Log.d("TAG", "starting restore")
             context.startService(intent)
         }
     }
--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -9,7 +9,6 @@ import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
 import android.provider.Telephony
 import android.util.*
 import android.view.View
@@ -18,21 +17,20 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.documentfile.provider.DocumentFile
-import androidx.documentfile.provider.DocumentFile.*
-import java.io.*
 
 const val FILE_SMS_BACKUP = "smsbackup"
 const val FILE_CONV_BACKUP = "convbackup"
 const val EXTRA_MESSAGE = "com.example.testapplication.MESSAGE"
 const val PERM_CODE_READ_SMS = 1
-const val SMS_FETCHED_ACTION = "com.example.testapplication.SMS_FETCHED"
+const val SMS_WRITTEN_ACTION = "com.example.testapplication.SMS_FETCHED"
+const val SMS_RESTORED_ACTION = "com.example.testapplication.SMS_RESTORED"
 const val SMS_FETCHING_UPDATE_ACTION = "com.example.testapplication.SMS_FETCHING"
+const val SMS_RESTORING_UPDATE_ACTION = "com.example.testapplication.SMS_RESTORING"
 const val ROLE_SMS_REQUEST_CODE = 2
 const val READ_DIR_REQUEST_CODE = 3
 
 val SMS_TABLE_TYPES = mapOf<String, Int>(
-    Telephony.Sms._ID to Cursor.FIELD_TYPE_INTEGER,
+//    Telephony.Sms._ID to Cursor.FIELD_TYPE_INTEGER,
 //    Telephony.TextBasedSmsColumns.THREAD_ID to Cursor.FIELD_TYPE_INTEGER,
     Telephony.TextBasedSmsColumns.ADDRESS to Cursor.FIELD_TYPE_STRING,
     Telephony.TextBasedSmsColumns.DATE_SENT to Cursor.FIELD_TYPE_INTEGER,
@@ -60,11 +58,15 @@ class MainActivity : AppCompatActivity() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val progressBar = findViewById<ProgressBar>(R.id.progressBar2)
             when (intent?.action) {
-                SMS_FETCHED_ACTION -> {
+                SMS_WRITTEN_ACTION -> {
                     Log.d("TAG", "FETCHED ACTION COMPLETED")
                     progressBar.progress = 100
                 }
-                SMS_FETCHING_UPDATE_ACTION -> {
+                SMS_RESTORED_ACTION -> {
+                    Log.d("TAG", "RESTORED ACTION COMPLETED")
+                    progressBar.progress = 100
+                }
+                SMS_FETCHING_UPDATE_ACTION, SMS_RESTORING_UPDATE_ACTION -> {
 //                    var receieved = intent.getFloatExtra(SMS_FETCHING_UPDATE_ACTION, 1f)
 //                    Log.d("TAG", "Received ${receieved}")
                     Thread(Runnable {
@@ -82,7 +84,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val btn = findViewById<Button>(R.id.button3)
         val filter = IntentFilter()
-        filter.addAction(SMS_FETCHED_ACTION)
+        filter.addAction(SMS_WRITTEN_ACTION)
         filter.addAction(SMS_FETCHING_UPDATE_ACTION)
         registerReceiver(broadcastReceiver, filter)
         btn.setOnClickListener {
@@ -90,7 +92,10 @@ class MainActivity : AppCompatActivity() {
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
                 val setSmsAppIntent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
                 setSmsAppIntent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, packageName)
-                startActivityForResult(setSmsAppIntent, CHANGE_SMS_DEFAULT_RESULT)
+                /**
+                 * TODO: Make sure this works
+                 */
+                startActivityForResult(setSmsAppIntent, ROLE_SMS_REQUEST_CODE)
             } else {
                 val roleManager = getSystemService(RoleManager::class.java)
                 if (roleManager != null && !roleManager.isRoleHeld(RoleManager.ROLE_SMS)) {
@@ -127,16 +132,6 @@ class MainActivity : AppCompatActivity() {
     fun sendMessage(@Suppress("UNUSED_PARAMETER")view: View) {
         // Do something in response to button
         Log.d("TAG", "going now")
-        if (FETCHING_SMS) {
-            Log.d("TAG", "fetching sms already taking place")
-        } else if (getDefaultSmsPackageName() != packageName) {
-            val setSmsAppIntent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
-            setSmsAppIntent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, packageName)
-            startActivity(setSmsAppIntent)
-            Log.d("TAG", "not default")
-        } else {
-            FetchMessages.startActionFoo(this, "", "")
-        }
 //        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
 //            putExtra(EXTRA_MESSAGE, msgData)
 //        }
@@ -144,7 +139,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun cursorTest(@Suppress("UNUSED_PARAMETER")view: View) {
-        val cursor = contentResolver.query(Telephony.Mms.CONTENT_URI, null, null, null, null)
+        val cursor = contentResolver.query(Uri.parse("content://mms/part"), null, "mid=80", null, null)
         if (cursor == null || !cursor.moveToFirst()) {
             Log.d("TAG", "null cursor or no data")
             return
@@ -162,8 +157,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun deleteMessages(@Suppress("UNUSED_PARAMETER")view: View) {
-        contentResolver.delete(Telephony.Sms.CONTENT_URI, null, null)
-//        contentResolver.delete(Telephony.Sms.Conversations.CONTENT_URI, null, null)
+         contentResolver.delete(Telephony.Sms.CONTENT_URI, null, null)
     }
 
     fun selectDirectory(@Suppress("UNUSED_PARAMETER")view: View) {
@@ -172,9 +166,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        Log.d("TAG", "$requestCode $resultCode")
-
         if (requestCode == ROLE_SMS_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 Log.d("TAG", "GRANTED")
@@ -192,184 +183,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun beginWrite(@Suppress("UNUSED_PARAMETER")view: View) {
-        writeToFile(pathToWriteTo, FILE_SMS_BACKUP)
-    }
-
-    private fun recordRows(cursor: Cursor, jsonWriter: JsonWriter, typeReference: Map<String, Int>) {
-        val total = cursor.count
-        var i = 0
-        do {
-            jsonWriter.beginObject()
-            for (idx in 0 until cursor.columnCount) {
-                val columnName = cursor.getColumnName(idx)
-                Log.d("TAG", "name: $columnName, value: ${cursor.getString(idx)}")
-                if (!typeReference.containsKey(columnName)) {
-                    continue
-                }
-                jsonWriter.name(columnName)
-                if (typeReference[columnName] == Cursor.FIELD_TYPE_INTEGER) {
-                    jsonWriter.value(cursor.getInt(idx))
-                } else {
-                    jsonWriter.value(cursor.getString(idx))
-                }
-            }
-            Log.d("TAG", "${i++}/$total")
-            jsonWriter.endObject()
-        } while (cursor.moveToNext())
-    }
-
-    private fun writeToFile(path: Uri, fileName: String) {
-//        try {
-//            var osw = OutputStreamWriter(openFileOutput())
-//        }
-        val rw = Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
-        if (rw) {
-            Log.d("TAG", "yes")
+        if (WRITING_SMS) {
+            Log.d("TAG", "fetching sms already taking place")
+        } else if (!checkPermission()) {
+            Log.d("TAG", "no permission, asking now")
+        } else if (pathToWriteTo == Uri.EMPTY) {
+            Log.d("TAG", "No path to write to")
         } else {
-            Log.d("TAG", "no")
-        }
-        if (pathToWriteTo == Uri.EMPTY) {
-            Log.d("TAG", "Empty")
-            return
-        }
-
-
-        val folderDocument = fromTreeUri(this, path)
-//        var files = folderDocument?.listFiles()
-//        if (files != null) {
-//            for (file in files) {
-//                Log.d("D","file found: ${file.name}")
-//            }
-//        }
-        if (folderDocument == null) {
-            Log.d("TAG", "folder does not exist")
-            return
-        }
-//        return
-
-        var foundFile = folderDocument.findFile("$fileName.json")
-        if (foundFile == null) {
-            foundFile = folderDocument.findFile(FILE_SMS_BACKUP) // Sometimes the json is not appended while writing
-        }
-        if (foundFile != null && !foundFile.delete()) {
-            Log.d("TAG", "file already exists and was unable to be deleted")
-            return
-        }
-        val fileURI = folderDocument.createFile("application/json", fileName)?.uri
-        if (fileURI == null) {
-            Log.d("TAG", "null file URI")
-            return
-        }
-        Log.d("TAG", "file path is ${fileURI.path}")
-        val outputStream = contentResolver.openOutputStream(fileURI)
-        if (outputStream == null) {
-            Log.d("TAG", "null output stream")
-            return
-        }
-        val jsonWriter = JsonWriter(OutputStreamWriter(outputStream))
-        val cursor = contentResolver.query(Telephony.Sms.CONTENT_URI, null, null, null, null)
-        if (cursor == null || !cursor.moveToFirst()) {
-            Log.d("tag", "null cursor")
-            return
-        }
-        val mmsCursor = contentResolver.query(Telephony.Mms.CONTENT_URI, null, null, null, null)
-        if (mmsCursor == null || !mmsCursor.moveToFirst()) {
-            Log.d("TAG", "null mms cursor")
-            return
-        }
-        Log.d("TAG", "write success")
-        jsonWriter.run {
-            beginObject()
-            name("sms")
-            beginArray()
-            recordRows(cursor, this, SMS_TABLE_TYPES)
-            cursor.close()
-            endArray()
-            name("mms")
-            beginArray()
-            recordRows(mmsCursor, this, MMS_TABLE_TYPES)
-            endArray()
-            endObject()
-            flush()
-            close()
+            FetchMessages.startActionWriteSMS(this, pathToWriteTo)
         }
     }
 
     fun beginRead(@Suppress("UNUSED_PARAMETER")view: View) {
-        readFile(pathToWriteTo, FILE_SMS_BACKUP, Telephony.Sms.CONTENT_URI, SMS_TABLE_TYPES)
+        if (RESTORING_SMS) {
+            Log.d("TAG", "reading sms already taking place")
+        } else if (getDefaultSmsPackageName() != packageName) {
+            val setSmsAppIntent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
+            setSmsAppIntent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, packageName)
+            startActivity(setSmsAppIntent)
+            Log.d("TAG", "not default")
+        } else if (pathToWriteTo == Uri.EMPTY) {
+            Log.d("TAG", "No path to write to")
+        } else {
+            FetchMessages.startActionRestoreSMS(this, pathToWriteTo)
+        }
     }
-
-    private fun readFile(path: Uri, fileName: String, contentUri: Uri, dataTypes: Map<String, Int>) {
-        // Store the column types by column name
-        if (!checkPermission()) {
-            return
-        }
-
-        if (path == Uri.EMPTY) {
-            Log.d("TAG", "Empty path")
-            return
-        }
-
-        // Open the file
-        val dir = fromTreeUri(this, path)
-        if (dir == null) {
-            Log.d("TAG", "document file null for read")
-            return
-        }
-
-        val file: DocumentFile? = dir.findFile("$fileName.json") ?: dir.findFile(fileName)
-        if (file == null) {
-            Log.d("TAG", "No file found")
-            return
-        }
-        val inputStream: InputStream? = contentResolver.openInputStream(file.uri)
-        if (inputStream == null) {
-            Log.d("TAG", "null input stream")
-            return
-        }
-        val rows: MutableList<ContentValues> = mutableListOf()
-        JsonReader(InputStreamReader(inputStream)).run {
-            beginArray()
-            while (hasNext()) {
-                beginObject()
-                val row = ContentValues()
-                while (hasNext()) {
-                    val key = nextName()
-                    if (!dataTypes.containsKey(key)) {
-                        skipValue()
-                        continue
-                    }
-
-                    if (dataTypes[key] == Cursor.FIELD_TYPE_INTEGER) {
-                        row.put(key, nextInt())
-                    } else if (peek() == JsonToken.NULL) {
-                        Log.d("TAG", "null token? $key")
-                        nextNull()
-                        row.putNull(key)
-                    } else {
-                        val value = nextString()
-                        Log.d("TAG", "Putting ${key}:${value}")
-                        row.put(key, value)
-                    }
-//                    Log.d("json", "key: ${nextName()}")
-//                    Log.d("json", "value: ${nextString()}")
-                }
-                endObject()
-                if (row.size() > 0) {
-                    rows.add(row)
-                }
-            }
-            endArray()
-            close()
-        }
-        val total = rows.count()
-        var i = 0
-        for (row in rows) {
-            Log.d("TAG", "Inserting ${i++}/$total")
-            contentResolver.insert(contentUri, row)
-
-        }
-        Log.d("TAG", "insertion complete")
-    }
-
 }
